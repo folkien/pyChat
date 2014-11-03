7c4ad170de1b61bf10a7e92d3d2513ff4148455f
folkien <folkus@gmail.com>
2014-11-03 12:01:38 +0100

    Master commit!

---
+++ pychat.py
@@ -0,0 +1,12 @@
+#pytanie o nazwe uzytkownika, jezeli taki user istnieje to ponownie pytamy
+
+#tworzenie nowego pliku sesji
+
+#zapamietanie timestampa sesji
+
+#Petla while command != /quit
+
+#jeden watek co kilka sekund sprawdza czy plik zostal zmodyfikowany
+
+#inny watek czeka na input os user'a
+
