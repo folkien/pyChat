8447d19079fa37dffe2a4b82e9d824a768329d49
spasz <folkus@gmail.com>
2014-11-03 23:16:16 +0100

    Pierwsza wersja tworzy plik sesji i tyle.

--- pychat.py
+++ pychat.py
@@ -1,7 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, datetime
+
+username = os.environ['USER']
+filename = "session@" + username
+
 #pytanie o nazwe uzytkownika, jezeli taki user istnieje to ponownie pytamy
 
-#tworzenie nowego pliku sesji
+if os.path.exists("./session/" + filename):
+    #To znaczy ze mamy juz uzytkownika o takiej nazwie.
+    print "Ops! Ktoś już ma taki login, musisz go zmienić na inny!"
+else:
+    #tworzenie nowego pliku sesji
+    session = open("./session/" + filename, 'w')
+    session.close();
 
+    #aktualny timestamp
+    actual_time = datetime.datetime.now()
+
+    #teraz bedziemy cyklicznie czytac czy plik zostal zmodyfikowany
+    
 #zapamietanie timestampa sesji
 
 #Petla while command != /quit
