2904db782b4c727967779e50f8a4b553f91502ef
folkien <folkus@gmail.com>
2014-11-04 11:15:56 +0100

    Juz mozna wybierac login oraz program tworzy pliki z sesja dla kazdego uzytkownika.

--- pychat.py
+++ pychat.py
@@ -1,24 +1,58 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os, datetime
+import os, datetime, glob
 
+
+#funkcje
+###################################################
+def writefile(filename,data):
+	f = open(filename,'a')
+	while f.closed:
+		f = open(filename,'a')
+	f.write(data)
+	f.close()
+
+def cleanfile(filename):
+	f = open(filename,'w')
+	while f.closed:
+		f = open(filename,'w')
+	f.close()
+
+def readfile(filename):
+	f = open(filename,'r')
+	while f.closed:
+		f = open(filename,'r')
+	data = f.read()
+	f.close()
+	return data
+
+def send(data):
+	users = glob.glob("./session/session@*");
+	for user in users:
+			writefile(user,data)
+###################################################
 username = os.environ['USER']
 filename = "session@" + username
 
 #pytanie o nazwe uzytkownika, jezeli taki user istnieje to ponownie pytamy
-
-if os.path.exists("./session/" + filename):
+sessionExists = os.path.exists("./session/" + filename)
+while sessionExists:
     #To znaczy ze mamy juz uzytkownika o takiej nazwie.
-    print "Ops! Ktoś już ma taki login, musisz go zmienić na inny!"
-else:
-    #tworzenie nowego pliku sesji
-    session = open("./session/" + filename, 'w')
-    session.close();
+	print "Ops! Ktoś już ma taki login, musisz go zmienić na inny!\n"
+	username = raw_input("Podaj swoj nowy login:") 
+	filename = "session@" + username
+	sessionExists = os.path.exists("./session/" + filename)
 
-    #aktualny timestamp
-    actual_time = datetime.datetime.now()
+#tworzenie nowego pliku sesji
+cleanfile("./session/" + filename)
 
-    #teraz bedziemy cyklicznie czytac czy plik zostal zmodyfikowany
+#aktualny timestamp
+actual_time = datetime.datetime.now()
+
+#wysylam informacje do innych uzytkownikow o zalogowaniu
+send("Użytkownik " + username + " zalogował się.\n")
+
+#teraz bedziemy cyklicznie czytac czy plik zostal zmodyfikowany
     
 #zapamietanie timestampa sesji
 
